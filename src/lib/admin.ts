e63b33fd2282e3a0096cfa372c869c404c029d37
Manav Jain <manavjain2210@gmail.com>
2024-08-07 21:00:05 -0700

    new updates

--- src/lib/admin.ts
+++ src/lib/admin.ts
@@ -5,8 +5,6 @@ var admin = require("firebase-admin");
 
 var serviceAccount = require("./service.json");
 
-
-
 getApps().length === 0 ? admin.initializeApp({
     credential: admin.credential.cert(serviceAccount),
     databaseURL: "https://new-2a630-default-rtdb.firebaseio.com"
